6eba11ef35490c2e17ebab4800602e67bb4de574
Lucas Hermann Negri <lucashnegri@gmail.com>
2014-12-14 17:23:29 -0200

    Changed the documentation theme.

--- docs/conf.py
+++ docs/conf.py
@@ -3,6 +3,7 @@
 #
 
 import peakutils
+from better import better_theme_path
 
 extensions = [
     'sphinx.ext.autodoc',
@@ -71,17 +72,8 @@ pygments_style = 'sphinx'
 
 # -- Options for HTML output ----------------------------------------------
 
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-html_theme = 'default'
-
-# Theme options are theme-specific and customize the look and feel of a theme
-# further.  For a list of options available for each theme, see the
-# documentation.
-#html_theme_options = {}
-
-# Add any paths that contain custom themes here, relative to this directory.
-#html_theme_path = []
+html_theme_path = [better_theme_path]
+html_theme = 'better'
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
