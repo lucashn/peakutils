0b4031d3c9e5ada6c243fb7772bce28d4f3fb067
Lucas Hermann Negri <lucashnegri@gmail.com>
2015-08-26 11:14:26 -0300

    Bump version.

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ with open('README.rst') as readme:
 
 setup(
     name='PeakUtils',
-    version='1.0.0',
+    version='1.0.1',
     description='Peak detection utilities for 1D data',
     author='Lucas Hermann Negri',
     author_email='lucashnegri@gmail.com',
