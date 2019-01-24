2f2cf4f3ad0d56ec9867f009ead22de170782e36
Lucas Hermann Negri <lucashnegri@gmail.com>
2019-01-24 12:12:07 -0300

    Version bump (1.3.2)

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.rst') as readme:
 
 setup(
     name='PeakUtils',
-    version='1.3.1',
+    version='1.3.2',
     description='Peak detection utilities for 1D data',
     long_description=long_description,
     long_description_content_type="text/x-rst",
